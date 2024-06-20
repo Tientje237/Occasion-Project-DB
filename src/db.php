b07732a124222f6df02d0cc375fe6fec70582740
TinovH <9019926@student.zadkine.nl>
2024-06-20 10:05:05 +0200

    Occasion DB inergration

---
+++ src/db.php
@@ -0,0 +1,32 @@
+<?php
+
+class DB
+{
+    private static $instance = null;
+    private $connection;
+
+    private $host = 'localhost';
+    private $dbname = 'Occasiondatabase';
+    private $username = 'root';
+    private $password = '';
+
+    private function __construct()
+    {
+        $this->connection = new PDO("mysql:host={$this->host};dbname={$this->dbname}", $this->username, $this->password);
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance == null) {
+            self::$instance = new DB();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+}
+
